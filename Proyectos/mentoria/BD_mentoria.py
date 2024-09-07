b41f77976075de3d2d57e3d3b64e74dff4fd1f4d
facundo0174 <Facundo-agustin@outlook.es>
2024-09-07 19:35:44 -0300

    teoria

---
+++ Proyectos/mentoria/BD_mentoria.py
@@ -0,0 +1,6 @@
+#BD mentoria
+import sqlite3
+import uuid #modulo de objetos permite funciones 
+import datetime
+
+
---
+++ Proyectos/mentoria/app mentoria.py
@@ -0,0 +1,5 @@
+#app mentoria
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+
+from BD_mentoria import DBHandlerNotas
